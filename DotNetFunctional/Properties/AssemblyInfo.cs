52c0fafd9e34631d2e56cf7c8ba7073ac55d0b66
Sir Phillip Tubell <sirphilliptubell@gmail.com>
2017-05-24 22:57:28 +0300

    fixed author name re-release and updated version number

--- DotNetFunctional/Properties/AssemblyInfo.cs
+++ DotNetFunctional/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DotNetFunctional")]
 [assembly: AssemblyDescription("A Functional / Monadic library for .Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("by Sir Phillip Tubell")]
+[assembly: AssemblyCompany("Sir Phillip Tubell")]
 [assembly: AssemblyProduct("DotNetFunctional")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
